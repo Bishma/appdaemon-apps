0b296160939e42573b786c2ec6d17cb966f230a4
Derek Rose <developer@dereksrose.com>
2024-05-24 19:18:33 -0700

    Harmony Hub replacement

--- apps/media_activity.py
+++ apps/media_activity.py
@@ -1,6 +1,7 @@
 import appdaemon.plugins.hass.hassapi as hass
 import yaml
 import time
+import urllib
 
 class MediaActivity(hass.Hass):
     def initialize(self):
@@ -9,14 +10,21 @@ class MediaActivity(hass.Hass):
         self.set_current_activity()
         self.log(self.current_activity, level = "DEBUG")
 
+        # Roku ECP API URL
+        self.roku_url = "http://"+self.args["roku_ip"]+":8060/"
+
         # Load media_activities.yaml
-        with open('/config/media_activities.yaml') as conf_file:
-            self.activity_conf = yaml.safe_load(conf_file);
+        with open('configs/media_sequences.yaml') as conf_file:
+            self.activity_conf = yaml.safe_load(conf_file)
             self.activities = self.activity_conf['activities']['living_room']
 
+        # Load the remote config
+        with open('/homeassistant/.storage/broadlink_remote_ec0baea35ab2_codes') as conf_file:
+            self.remote_conf = yaml.safe_load(conf_file)
+
         # triggers
-        self.activity_changer("input_text.lr_remote_activity",'state','off','roku', '')
-        #self.listen_state(self.activity_changer, entity_id="input_text.lr_remote_activity")
+        self.listen_state(self.activity_changer, entity_id="input_text.lr_remote_activity")
+        self.listen_event(self.button_press, "remote_button_press")
 
     def set_current_activity(self):
         # Set a global tracking variable for the current media activity
@@ -29,14 +37,12 @@ class MediaActivity(hass.Hass):
 
         self.log('Activity change from '+old+' to '+new, level = "INFO")
 
-        if new not in self.activity_conf['activities']['living_room']:
-            # if the requested activity isn't valid, reset the current activity tracker and return false
-            self.set_current_activity(old)
+        if new in self.activity_conf['activities']['living_room']:
+            diffs = self.activity_diff_finder(self.activities[new], self.activities[old])
+            self.command_sender(diffs)
+        else:
+            self.error('Invalid activity: '+new)
             return False
-
-        diffs = self.activity_diff_finder(self.activities[new], self.activities[old])
-
-        self.command_sender(diffs)
 
     def activity_diff_finder(self, new_conf, old_conf):
         # This takes the config stub for the new activity and compares it to the old.
@@ -47,7 +53,6 @@ class MediaActivity(hass.Hass):
         for device in new_conf.keys():
             # Loop through all the things that are in the new activity's conf
             for desired_state in new_conf[device]['sequence']:
-                #print(desired_state)
                 current_state= next((seq for seq in old_conf[device]['sequence'] if seq['id'] == desired_state['id']),{})
                 if ('state' in current_state and desired_state['state'] != current_state['state']) or 'state' not in current_state:
                     # If the state needs to change or there was no former state
@@ -57,23 +62,113 @@ class MediaActivity(hass.Hass):
                         self.log(device+' should be '+desired_state['state'], level='DEBUG')
                     
                     command = {
-                        'device': new_conf[device]['device'],
-                        'command': desired_state['command']
+                        'device_id': new_conf[device]['device_id'],
+                        'button_id': desired_state['button_id']
                     }
                     detected_diff.append(command)
 
-                    # special check to see if there needs to be a pause before moving on
-                    if 'wait' in desired_state:
-                        self.log('A pause should go here', level="DEBUG")
-                        detected_diff.append({ 'device': 'wait', 'command': 12 })
-            
         return detected_diff
 
+    def button_press(self, event, data, kwargs):
+        self.log("Event: "+event, level = "DEBUG")
+
+        # Support for pre-scripted sequences
+        commands = []
+        if data["device_id"] == "sequence":
+            # get the sequence from the config
+            sequence = self.activity_conf['sequences'][data["button_id"]]
+            self.log("Sequence: "+str(sequence), level = "DEBUG")
+
+            for command in sequence:
+                # loop through the sequence and add the commands to the list
+                # I zero-indexed the repeats for grammatical purposes, so add one to make it work in loops
+                repeats = command.get("repeats", 0)  # Get the number of repeats, default to 0 if not present
+                repeats += 1
+                for i in range(repeats):
+                    commands.append({"device_id": command["device_id"], "button_id": command["button_id"]})
+        # Non-sequence commands
+        else:
+            # I zero-indexed the repeats for grammatical purposes, so add one to make it work in loops
+            repeats = data.get("repeats", 0)  # Get the number of repeats, default to 0 if not present
+            repeats += 1 # add one to the repeats because it's always one less than it should be
+
+            # If repeating more than once, send the command multiple times via the command_sender's list support
+            for i in range(repeats):
+                commands.append({ "device_id": data["device_id"], "button_id": data["button_id"]})
+
+        self.command_sender(commands)
+
     def command_sender(self, commands):
+        self.log("Issuing {} commands".format(len(commands)), level = "DEBUG")
         for command in commands:
-            if command['device'] == 'wait':
-                self.log("Wait encounted, sleeping for "+str(command['command'])+ " seconds")
-                #time.sleep(command['command'])
+            device = str(command['device_id'])
+            command = str(command['button_id'])
+            self.log("Sending command: "+command+" to "+device)
+            
+            # Call the appropriate function to send the command
+            if device == "roku":
+                self.roku_sender(command)
             else:
-                # send command via HA to the remote
-                self.log("Sending "+str(command['command'])+" sent to "+str(command['device']))
+                self.ir_sender(device, command)
+
+            if device == "visiotv" and command == "powerOn":
+                self.log("Pausing for the TV to be stupid.")
+                time.sleep(12)
+
+            time.sleep(0.33)
+
+    def ir_sender(self, device_id, button_id, unit_id = "living_room"):
+        # Send an IR command to a device
+        # device_id is the ID of the device in the harmony config file
+        # button_id is the ID of the button in the harmony config file
+        # repeats is the number of times to send the command
+
+        entity_id = "remote."+unit_id+"_mantle_ir_remote"
+
+        if device_id in self.remote_conf['data'] and button_id in self.remote_conf['data'][device_id]:
+            self.log("Sending IR command: "+button_id+" to "+device_id, level = "DEBUG")
+            self.call_service("remote/send_command", entity_id=entity_id, device=device_id, command=button_id)
+        else:
+            self.error("Invalid button: " + button_id)
+
+    def roku_sender(self, button_id, unit_id = "living_room"):
+        # Send a Roku command to the Roku
+        # button_id is the ID of the button in the Roku API
+        # repeats is the number of times to send the command
+
+        # From: https://www.home-assistant.io/integrations/roku/
+        available_buttons = [
+            "back",
+            "down",
+            "enter",
+            "forward",
+            "home",
+            "info",
+            "left",
+            "play",
+            "replay",
+            "reverse",
+            "right",
+            "select",
+            "up",
+            "volume_mute"
+        ]
+
+        entity_id = "remote."+unit_id
+
+        if button_id in available_buttons:
+            self.log("Sending Roku command: "+button_id, level = "DEBUG")
+            self.call_service("remote/send_command", entity_id=entity_id, command=button_id)
+            
+            # To use the roku API directly:
+            # You'll need to change available buttons to match: https://developer.roku.com/docs/developer-program/dev-tools/external-control-api.md#keypress-key-values
+            #self.log("Sending Roku command: "+button_id+" to "+self.roku_url+"keypress/"+button_id)
+            #try:
+            #    urllib.request.urlopen(self.roku_url+"keypress/"+button_id, timeout=1)
+            #except Exception:
+            #    import traceback
+            #    self.error('Unable to send keypress to Roku')
+            #    self.error('generic exception: ' + traceback.format_exc())
+            #urllib.request.urlopen(self.roku_url+"keypress/"+button_id, timeout=1)
+        else:
+            self.error("Invalid button: " + button_id)
--- apps/roku_channels.py
+++ apps/roku_channels.py
@@ -10,10 +10,13 @@ class RokuChannels(hass.Hass):
         self.roku_channel_def = {} # Channel dict that equates name to channel ID
         self.roku_channel_list = [] # List of channel names from the roku endpoint
         self.ha_channel_list = [] # The list currently stored in HA
-        self.channel_url = "http://192.168.68.134:8060/query/apps"
+        self.channel_url = "http://"+self.args['roku_ip']+":8060/query/apps"
         self.change_lock = False
         
         self.run_every(self.update_channels, "now", 5 * 60)
+
+        # TODO: For each channel, set up a listener
+        # TODO: Remove this listener
         self.listen_state(self.channel_changer, entity_id="input_select.roku_channels")
 
         #print(json.dumps(self.roku_channel_def, indent=2))
@@ -31,9 +34,12 @@ class RokuChannels(hass.Hass):
         # diff with the new channel list
         if self.roku_channel_list != self.ha_channel_list:
             # If there's a difference update home assistant
+
             self.call_service("input_select/set_options", entity_id="input_select.roku_channels", options=self.roku_channel_list)
             self.set_input_select()
             self.log('Channels Updated')
+
+        self.create_switches()
 
         self.set_input_select()
 
@@ -41,6 +47,18 @@ class RokuChannels(hass.Hass):
         # Set the default option for the input select
         current_app = self.entities.media_player.living_room.attributes.app_name
         self.call_service("input_select/select_option", entity_id="input_select.roku_channels", option=current_app)
+
+    def create_switches(self):
+        # Create a switch for each channel
+        for channel in self.roku_channel_list:
+            channel_name = channel.split('-', 1)[0].strip()
+            entity_name = "switch.lr_roku_"+channel_name.lower().replace(" ", "_")
+
+            # Test to see with the switch entity exists
+            if not self.entity_exists(entity_name):
+                # Create a swtich entity for each channel
+                self.log("Creating switch entity for "+entity_name)
+                self.set_state(entity_name, state="off", attributes={"friendly_name": "Roku "+channel_name})
     
     def load_roku_channels(self):
         # Get file from url
@@ -55,6 +73,7 @@ class RokuChannels(hass.Hass):
             self.roku_channel_def = {app['#text'] : app['@id'] for app in raw_channels['apps']['app']}
             self.roku_channel_def = OrderedDict(sorted(self.roku_channel_def.items()))
             self.roku_channel_list = list(self.roku_channel_def.keys())
+            self.roku_channel_list.insert(0, "Home")
 
     def load_ha_channels(self):
         self.ha_channel_list = self.entities.input_select.roku_channels.attributes.options
@@ -64,6 +83,8 @@ class RokuChannels(hass.Hass):
             self.change_lock = True
 
     def channel_changer(self, entity, attribute, old, new, kwargs):
+        # TODO: Needs to reset switches
+        # TODO: Send Off, Record, OTA, and Switch to the media_activity app
         self.log("channel changer called with "+new+" and "+old)
         # catch the roku_channel input select changing
         if (self.change_lock == True):
